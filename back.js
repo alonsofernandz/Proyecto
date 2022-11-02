b466821fac4cf4bd37f038623355060fcecae9db
Alonso <66333686+AlonsoFJ26@users.noreply.github.com>
2022-11-02 13:49:38 -0500

    Avance Final

--- back.js
+++ back.js
@@ -4,5 +4,4 @@ function nameusuario(){
     let nom = document.getElementById("name").value;
     let pass = document.getElementById("pasww").value;
     alert("Bienvenido " + nom);
-    console.log("Tu contraseña es : "+pass);
 }
